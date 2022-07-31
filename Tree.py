18c1c28f52f24547403e44ab2680ab0c5044ac96
Atharva Suryawanshi <87293779+Atharvasurya@users.noreply.github.com>
2022-07-31 14:49:23 +0530

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,39 @@
+import turtle
+import random
+
+BRANCH_COUNT = 7
+TURN_ANGLE = 10
+SHRINK_FACTOR = 0.8
+
+def tree(size, depth):
+  if depth >= 1:
+    if random.random() > 0.33:
+      turt.width(depth)
+      turt.color(get_color(depth))
+      turt.forward(size)
+      turt.right(TURN_ANGLE)
+      tree(size*SHRINK_FACTOR, depth-1)
+      turt.right(TURN_ANGLE)
+      tree(size*SHRINK_FACTOR, depth-1)
+      turt.left(3*TURN_ANGLE)
+      tree(size*SHRINK_FACTOR, depth-1)
+      turt.left(TURN_ANGLE)
+      tree(size*SHRINK_FACTOR, depth-1)
+      turt.right(2*TURN_ANGLE)
+      turt.color(get_color(depth))
+      turt.backward(size)
+
+def get_color(depth):
+  if BRANCH_COUNT - depth < 3:
+    return 'brown'
+  else:
+    return 'green'
+
+if __name__ == '__main__':
+  random.seed(46)
+  turt = turtle.Turtle()
+  turt.setheading(90)
+  turt.speed(0)
+  turtle.colormode(255)
+  tree(100, BRANCH_COUNT)
+  turtle.done()
